c044230ccd4d29ff87a69109728e4bb5b58e81d8
GoSeungHyeon <rhtld123@gmail.com>
2022-10-09 16:31:49 +0900

    initial commit

---
+++ src/main/java/com/go/conowithme/ConoWithMeApplication.java
@@ -0,0 +1,13 @@
+package com.go.conowithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConoWithMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConoWithMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/go/conowithme/ConoWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.go.conowithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConoWithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
