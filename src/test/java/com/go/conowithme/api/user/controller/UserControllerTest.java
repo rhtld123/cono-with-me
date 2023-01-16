366e5197bd7c02c69f9ad180a3ae1b4fb075f464
rhtld123 <rhtld123@gmail.com>
2023-01-16 23:30:23 +0900

    테스트 오류 수정

--- src/test/java/com/go/conowithme/api/user/controller/UserControllerTest.java
+++ src/test/java/com/go/conowithme/api/user/controller/UserControllerTest.java
@@ -4,7 +4,13 @@ import com.go.conowithme.api.user.domain.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("local")
 class UserControllerTest {
 
     @Autowired
