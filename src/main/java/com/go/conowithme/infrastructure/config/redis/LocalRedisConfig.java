0b0b1dacb59a961b2c403fdde5759969a58b5de6
rhtld123 <rhtld123@gmail.com>
2023-01-16 23:30:02 +0900

    LocalRedisConfig test시 포트 변경해서 실행하도록 변경

--- src/main/java/com/go/conowithme/infrastructure/config/redis/LocalRedisConfig.java
+++ src/main/java/com/go/conowithme/infrastructure/config/redis/LocalRedisConfig.java
@@ -1,9 +1,6 @@
 package com.go.conowithme.infrastructure.config.redis;
 
-import java.io.File;
-import java.io.IOException;
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +10,13 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @Profile("local")
 @Configuration
@@ -25,10 +29,11 @@ public class LocalRedisConfig {
 
     @PostConstruct
     public void redisServer() throws Exception {
+        int port = isRedisRunning() ? findAvailablePort() : redisPort;
         if (isArmMac()) {
-            redisServer = new RedisServer(getRedisFileForArmMac(), redisPort);
+            redisServer = new RedisServer(getRedisFileForArmMac(), port);
         } else {
-            redisServer = RedisServer.builder().port(redisPort).setting("maxmemory 128M").build();
+            redisServer = RedisServer.builder().port(port).setting("maxmemory 128M").build();
         }
         redisServer.start();
     }
@@ -42,12 +47,62 @@ public class LocalRedisConfig {
 
     private boolean isArmMac() {
         return System.getProperty("os.arch").equals("aarch64") &&
-            System.getProperty("os.name").equals("Mac OS X");
+                System.getProperty("os.name").equals("Mac OS X");
     }
 
     private File getRedisFileForArmMac() throws IOException {
         File file = new ClassPathResource("redis/redis-server-m1-mac").getFile();
         return file;
+    }
+
+    /**
+     * Embedded Redis가 현재 실행중인지 확인
+     */
+    private boolean isRedisRunning() throws IOException {
+        return isRunning(executeGrepProcessCommand(redisPort));
+    }
+
+    /**
+     * 현재 PC/서버에서 사용가능한 포트 조회
+     */
+    public int findAvailablePort() throws IOException {
+
+        for (int port = 10000; port <= 65535; port++) {
+            Process process = executeGrepProcessCommand(port);
+            if (!isRunning(process)) {
+                return port;
+            }
+        }
+
+        throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
+    }
+
+    /**
+     * 해당 port를 사용중인 프로세스 확인하는 sh 실행
+     */
+    private Process executeGrepProcessCommand(int port) throws IOException {
+        String command = String.format("netstat -nat | grep LISTEN|grep %d", port);
+        String[] shell = {"/bin/sh", "-c", command};
+        return Runtime.getRuntime().exec(shell);
+    }
+
+    /**
+     * 해당 Process가 현재 실행중인지 확인
+     */
+    private boolean isRunning(Process process) {
+        String line;
+        StringBuilder pidInfo = new StringBuilder();
+
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+
+            while ((line = input.readLine()) != null) {
+                pidInfo.append(line);
+            }
+
+        } catch (Exception e) {
+        }
+
+        return !StringUtils.isEmpty(pidInfo.toString());
     }
 
     @Bean
